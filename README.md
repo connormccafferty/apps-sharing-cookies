64c94c00b6274cd4b8cffe7f5845318e2f40eb69
connormccafferty <connor.mccafferty@openfin.co>
2020-10-28 16:15:41 -0400

    adjusted for premium and lite app case, on /login fetch send isPremium to client

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-### Sharing cookies between Applications
+### Login App Launch Example
 
-App's on the same runtime use the same cookie / session store thus you get cookie sharing by default. In this example you can set a cookie with the `login` button, once "logged in" you'll notice when launching apps via `startFromManifest` they have the cookie - if not logged in the apps will open back at the `login.html` page.
+App's on the same runtime use the same cookie / session store by default. This example launches a login page application `app.login.json`. "login" by entering a username and pressing the "login" button. No matter what you enter a cookie will be set by the server and another application will launch. If you enter a username that includes `@openfin.co` the "premium" app `app.premium.json` will launch.
 
 #### Assumptions
 
-- apps are on the same runtime (and security realm if applicable)
-- manifests aren't on protected routes (i.e. cookies/sessions won't be present on the request for the manifest itself because the RVM is making that request)
+- all apps (`app.login.json`, `app.lite.json`, `app.premium.json` are on the same runtime (and security realm if applicable)
+- the manifests aren't behind authentication (i.e. cookies/sessions won't be present on the request for the manifest itself because it's being made by the RVM)
 
 #### How to use
 
 - clone the repository: `git clone https://github.com/connormccafferty/apps-sharing-cookies.git`
 - install dependancies: `cd apps-sharing-cookies` && `npm i`
-- start the server: `npm start`
-- start the app: `npm run start:openfin`
+- start the server / launch openfin: `npm start`
--- index.js
+++ index.js
@@ -1,33 +1,68 @@
 const path = require("path");
 const express = require("express");
+const { launch, connect } = require("hadouken-js-adapter");
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const app = express();
 
 const PORT = 5555;
 
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  // check for cookie
-  if (req.cookies["cookie"]) {
+  // no cookie = not "logged in"; send back to login page
+  if (req.cookies["token"]) {
     res.sendFile(path.join(__dirname + "/public/app.html"));
   } else {
     res.sendFile(path.join(__dirname + "/public/login.html"));
   }
 });
 
-app.get("/signin", (req, res) => {
-  // set cookie
-  var randomNumber = Math.random().toString();
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+  let isPremium = false;
+
+  // if the username includes and openfin email we'll consider this a "premium" user
+  if (username && username.includes("@openfin.co")) {
+    isPremium = true;
+  }
+
+  // we will use this for the cookie
+  let randomNumber = Math.random().toString();
   randomNumber = randomNumber.substring(2, randomNumber.length);
-  res.cookie("cookie", randomNumber, {
-    maxAge: 900000,
-    httpOnly: true,
-  });
+
+  // set a cookie to indicate "authenticated" and send isPremium value
+  // the client (login page) will use isPremium to determine which app to launch
+  res
+    .cookie("token", randomNumber, {
+      maxAge: 900000,
+      httpOnly: true,
+    })
+    .send({ isPremium });
+
   res.end();
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`server is listening on ${PORT}`);
+  const manifestUrl = `http://localhost:${PORT}/app.login.json`;
+
+  try {
+    //Once the server is running we can launch OpenFin and retrieve the port.
+    const port = await launch({ manifestUrl });
+
+    //We will use the port to connect from Node to determine when OpenFin exists.
+    const fin = await connect({
+      uuid: "server-connection", //Supply an addressable Id for the connection
+      address: `ws://localhost:${port}`, //Connect to the given port.
+      nonPersistent: true, //We want OpenFin to exit as our application exists.
+    });
+
+    //Once OpenFin exists we shut down the server.
+    fin.once("disconnected", process.exit);
+  } catch (err) {
+    console.error(err);
+  }
 });
